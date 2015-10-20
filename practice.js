0e2597254f1c9b2084eadaa32ea0d5e4262f9326
Austin Vance <austinvance@Austin-Vances-MacBook-Pro.local>
2015-10-20 13:41:09 -0600

    keyword this project

--- practice.js
+++ practice.js
@@ -2,6 +2,8 @@
   // 1) What is the purpose of the 'this keyword'?
 
       //Answer
+// The this keyword allows an object's properties to be accessed
+// without having to restate the objects name each time you want to access a property.
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
@@ -10,11 +12,15 @@
   // 3) What is the difference between call and apply?
 
       //Answer
+Call and apply do the same thing and differ only in the 
+argument that is passed when calling the function.
+For example, f.apply(o, [1, 2]) vs. f.call(o, 1, 2)
 
   // 4) What does .bind do?
 
       //Answer
-
+// Saying var newF = f.bind(someObj) makes a copy of the function, f, in which all
+// 'this' values are permanently set to the keys of the object in the argument.
 
 //Next Problem
 
@@ -24,16 +30,32 @@
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
     //Code Here
+var user = {
+  username: 'Austin',
+  email: 'austinv93@yahoo.com',
+  getUsername: function() {
+    return this.username;
+  }
+}
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
 
 
 //Next Problem
+console.log(user.getUsername());
 
 
 // Write the function definitions which will make the following function invocations function properly.
 
   //Function Invocations Here
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.newCar = function() {
+    this.move += 10;
+  };
+}
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
@@ -54,7 +76,10 @@ var getYear = function(){
 //Above you're given the getYear function. Using your prius and mustang objects from above, use the proper syntax that will allow for you to call the getYear function with the prius then the mustang objects being the focal objects. *Don't add getYear as a property on both objects*.
 
   //Code Here
-
+console.log(getYear.call(prius));
+console.log(getYear.apply(prius));
+console.log(getYear.call(mustang));
+console.log(getYear.apply(mustang));
 
 
 //New Problem
